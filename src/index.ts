53aae06525db7104ff0c6ea63c9c6e7bf4b1faa4
Ben Ross <bross@indigoag.com>
2018-04-12 22:36:14 -0400

    feat(initial): Initial commit

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+import { ArgumentParser } from 'argparse';
+import * as AWS from 'aws-sdk';
+import { CloudFormation, Config } from 'aws-sdk';
+import { DescribeStacksOutput } from 'aws-sdk/clients/cloudformation';
+
+import { Parser } from './parse';
+import { ResolverMap } from './types';
+
+const resolvers: ResolverMap = {
+  cft: async (argument: string) => {
+    if (!AWS.config.region) {
+      AWS.config.update({ region: 'us-east-1' });
+    }
+    const cft = new CloudFormation();
+    const parsedArgument = argument.split('.', 2);
+    let stack: DescribeStacksOutput;
+    try {
+      stack = await cft.describeStacks({ StackName: parsedArgument[0] }).promise();
+    } catch (e) {
+      throw new Error(`Could not get info for stack with name ${parsedArgument[0]} when parsing cft reference ${argument}: ${e}`);
+    }
+    for (const output of stack.Stacks[0].Outputs) {
+      if (output.OutputKey === parsedArgument[1]) {
+        return output.OutputValue;
+      }
+    }
+    throw new Error(`Could not locate output ${parsedArgument[1]} of stack ${parsedArgument[0]}`);
+  },
+  env: async (argument: string) => {
+    return process.env[argument];
+  },
+  const: async (argument: string) => {
+    return argument;
+  }
+}
+
+const parser = new ArgumentParser();
+parser.addArgument(['-s', '--stage'], {
+  help: 'Environment stage',
+  dest: 'stage'
+});
+parser.addArgument(['file'], {
+  help: 'env.yml file'
+});
+const args = parser.parseArgs();
+
+
+try {
+  // TODO Load external resolvers
+
+  let document = yaml.safeLoad(fs.readFileSync(args.file, 'utf8'));
+  if (args.stage) {
+    document = document[args.stage];
+    if (!document) {
+      throw new Error(`Could not locate stage ${args.stage} in file ${args.file}`);
+    }
+  }
+  const parser = new Parser(resolvers);
+  parser.parse(document).then(result => {
+    console.info(JSON.stringify(result, undefined, 2));
+  });
+} catch (e) {
+  console.error(`Could not load yaml ${e}`);
+}
---
+++ src/parse.ts
@@ -0,0 +1,36 @@
+import { ResolverMap } from './types';
+
+export class Parser {
+  constructor(private resolvers: ResolverMap) { }
+
+  async parse(document: any): Promise<any> {
+    const variables = Object.keys(document);
+    const result = {};
+    for (const variable of variables) {
+      const value = document[variable];
+      result[variable] = await this.parseValue(value);
+    }
+    return result;
+  }
+
+  private async parseValue(value: string): Promise<string> {
+    // TODO Handle parsing embedded references
+    const regex = new RegExp('\\${([a-z]+):(.*)}');
+    const results = value.match(regex);
+    const resolverName = results && results[1];
+    const innerValue = results ? results[2] : value;
+    const resolver = this.getResolver(resolverName);
+    if (!resolver) {
+      throw new Error(`Could not locate resolver for value ${value}`);
+    }
+    const result = await resolver(innerValue);
+    return result;
+  }
+
+  private getResolver(name: string) {
+    if (!name) {
+      return this.resolvers['const'];
+    }
+    return this.resolvers[name];
+  }
+}
