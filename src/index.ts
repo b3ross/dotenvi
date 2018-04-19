5594ba189a612fd58f883cd452ca9333ff64c41c
Ben Ross <bross@indigoag.com>
2018-04-19 18:23:49 -0400

    fix(tests): Refactor for tests

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,10 @@
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
 import { ArgumentParser } from 'argparse';
-import * as AWS from 'aws-sdk';
-import { CloudFormation, Config } from 'aws-sdk';
-import { DescribeStacksOutput } from 'aws-sdk/clients/cloudformation';
 
-import { Parser } from './parse';
-import { ResolverMap } from './types';
-
-const resolvers: ResolverMap = {
-  cft: async (argument: string) => {
-    if (!AWS.config.region) {
-      AWS.config.update({ region: 'us-east-1' });
-    }
-    const cft = new CloudFormation();
-    const parsedArgument = argument.split('.', 2);
-    let stack: DescribeStacksOutput;
-    try {
-      stack = await cft.describeStacks({ StackName: parsedArgument[0] }).promise();
-    } catch (e) {
-      throw new Error(
-        `Could not get info for stack with name ${parsedArgument[0]} when parsing cft reference ${argument}: ${e}`
-      );
-    }
-    for (const output of stack.Stacks[0].Outputs) {
-      if (output.OutputKey === parsedArgument[1]) {
-        return output.OutputValue;
-      }
-    }
-    throw new Error(`Could not locate output ${parsedArgument[1]} of stack ${parsedArgument[0]}`);
-  },
-  env: async (argument: string) => {
-    return process.env[argument];
-  },
-  constant: async (argument: string) => {
-    return argument;
-  }
-};
+import { Rewriter } from './rewriter';
+import { ResolverMap, Document } from './types';
+import { resolvers } from './resolvers';
 
 const parser = new ArgumentParser();
 parser.addArgument(['-s', '--stage'], {
@@ -59,15 +27,15 @@ function writeFile(document: { [name: string]: string }) {
 try {
   // TODO Load external resolvers
 
-  let document = yaml.safeLoad(fs.readFileSync('./env.yml', 'utf8'));
+  let document = yaml.safeLoad(fs.readFileSync('./env.yml', 'utf8')) as Document;
   if (args.stage) {
     document = (document as any)[args.stage];
     if (!document) {
       throw new Error(`Could not locate stage ${args.stage} in file ${args.file}`);
     }
   }
-  const parser = new Parser(resolvers);
-  parser.parse(document).then(result => {
+  const rewriter = new Rewriter(resolvers);
+  rewriter.rewrite(document).then(result => {
     writeFile(result);
   });
 } catch (e) {
