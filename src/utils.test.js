38869088535e5b8cd2a5393fb4577f1c2e5c3b11
Ben Ross <bross@indigoag.com>
2018-04-21 20:44:38 -0400

    feat(validation): add validation for required configuration

---
+++ src/utils.test.js
@@ -0,0 +1,45 @@
+const { validateOutput } = require('./utils');
+
+describe('validateOutput', () => {
+  it('Validates successful output', () => {
+    const input = {
+      'variable': {
+        value: 'constant'
+      },
+      'optional': {
+        value: undefined,
+        optional: true
+      }
+    };
+    const output = {
+      'variable': 'constant',
+    };
+    const errors = validateOutput(input, output);
+    expect(errors.length).toBe(0);
+  });
+
+  it('Errors if output null and required', () => {
+    const input = {
+      'environment': {
+        value: '${env:this_should_not_be_defined}'
+      }
+    };
+    const output = {
+    }
+    const errors = validateOutput(input, output);
+    expect(errors.length).toBeGreaterThan(0);
+  });
+
+  it('Does not error if output null and optional', () => {
+    const input = {
+      'environment': {
+        value: '${env:this_should_not_be_defined',
+        optional: true
+      }
+    };
+    const output = {
+    }
+    const errors = validateOutput(input, output);
+    expect(errors.length).toBe(0);
+  });
+});
